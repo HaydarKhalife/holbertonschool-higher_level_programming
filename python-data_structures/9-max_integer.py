84030f5420ab6de60109784fc93400e8227ccb71
HaydarKhalife <146635050+HaydarKhalife@users.noreply.github.com>
2024-01-27 23:29:58 +0200

    Update 9-max_integer.py

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,11 +1,5 @@
 #!/usr/bin/python3
-def max_integer(my_list=[]):
-    if not my_list:
-        return None
-
-    max_n = min(my_list)
-    for i in my_list:
-        if i > max_n:
-            max_n = i
-
-    return max_n
+a = 89
+b = 10
+a, b = b, a
+print("a={:d} - b={:d}".format(a, b))

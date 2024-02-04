5a05b423abe4951f38f622b2303cde45affdabdb
HaydarKhalife <146635050+HaydarKhalife@users.noreply.github.com>
2024-02-04 14:16:39 +0200

    Update 1-square.py

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,21 +1,8 @@
 #!/usr/bin/python3
-"""Square Class
-
-A Square Class
-
-"""
+'''a class called square is initialized'''
 
 
 class Square:
-
+    '''a private instance is made to an attribute with the name size'''
     def __init__(self, size):
-        """__init__
-
-        The __init__ method initializes the size value
-        of the square.
-
-        Attributes:
-            size (int): The size of the square.
-
-        """
         self.__size = size

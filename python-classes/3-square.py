ff4128636a1a7ca93b95282a33bb0309f26dfb15
HaydarKhalife <146635050+HaydarKhalife@users.noreply.github.com>
2024-02-04 14:18:29 +0200

    Update 3-square.py

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,36 +1,17 @@
 #!/usr/bin/python3
+'''a class called square is initialized'''
+
+
 class Square:
-    """Square Class
-
-    A Square Class
-
-    """
-
+    '''a private instance is made to an attribute with the name size'''
     def __init__(self, size=0):
-        """__init__
-
-        The __init__ method initializes the size value of the square.
-
-        Attributes:
-            size (:obj:`int`, optional): The size of the square.
-
-        Raises:
-            TypeError: If `size` type is not `int`.
-
-            ValueError: If `size` is less than `0`.
-
-        """
-
-        if type(size) is not int:
-            raise TypeError('size must be an integer')
-
-        if size < 0:
-            raise ValueError('size must be >= 0')
-
         self.__size = size
-
+        '''raise an error if the data type is incorrect'''
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        '''raise an error if the data is less that 0'''
+        if size < 0:
+            raise ValueError("size must be >= 0")
+    '''returns the current square area'''
     def area(self):
-        """Returns the current square area
-
-        """
-        return self.__size ** 2
+        return self.__size * self.__size

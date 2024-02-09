28d6fb2ff138a93cb97279ca0c2fc60d32a7c64f
HaydarKhalife <146635050+HaydarKhalife@users.noreply.github.com>
2024-02-09 20:42:39 +0200

    Update 10-square.py

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,16 +1,10 @@
 #!/usr/bin/python3
-"""Square Class"""
-
-
-Rectangle = __import__('9-rectangle').Rectangle
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """Square Class"""
-
     def __init__(self, size):
-        """Instantiation"""
-        super().integer_validator("size", size)
+        self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size

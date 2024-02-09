7db2330658f7dd700db0dcb66c3615b0490be69e
HaydarKhalife <146635050+HaydarKhalife@users.noreply.github.com>
2024-02-09 20:22:12 +0200

    Update 0-lookup.py

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-def lookup(obj):
-    """
-    Returns the list of available attributes and methods of an object
-    """
+"""return the list of available attributes
+and methods of an object"""
 
+
+def lookup(obj):
+    """returns list of available
+        attributes and methods of an object"""
     return dir(obj)

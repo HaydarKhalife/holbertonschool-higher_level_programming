bc3a87e1686146ccc0a81468dd2f5808f3df682d
HaydarKhalife <146635050+HaydarKhalife@users.noreply.github.com>
2024-02-10 22:19:28 +0200

    Update 101-stats.py

--- python-input_output/101-stats.py
+++ python-input_output/101-stats.py
@@ -1,41 +1,49 @@
 #!/usr/bin/python3
+"""
+Script reads stdin line by line and computes metrics
+
+Input format:
+<IP Address> - [<date>] "GET /projects/260 HTTP/1.1" <status code> <file size>
+
+Each 10 lines and after a keyboard interruption (CTRL + C),
+prints those statistics since the beginning:
+total file size and
+possible status code: 200, 301, 400, 401, 403, 404, 405 and 500
+
+format: File size: <total size>
+format: <status code (in ascending order)>: <number>
+"""
+
+
 import sys
 
-def print_metrics(total_size, status_codes):
-    """Print metrics based on total size and status codes."""
-    print("File size: {:d}".format(total_size))
-    for code in sorted(status_codes.keys()):
-        print("{}: {}".format(code, status_codes[code]))
 
-def parse_line(line, total_size, status_codes):
-    """Parse line and update metrics."""
+def print_size_and_codes(size, stat_codes):
+    print("File size: {:d}".format(size))
+    for k, v in sorted(stat_codes.items()):
+        if v:
+            print("{:s}: {:d}".format(k, v))
+
+
+def parse_stdin_and_compute():
+    size = 0
+    lines = 0
+    stat_codes = {"200": 0, "301": 0, "400": 0, "401": 0,
+                  "403": 0, "404": 0, "405": 0, "500": 0}
     try:
-        parts = line.split()
-        size = int(parts[-1])
-        status_code = parts[-2]
-        total_size += size
-
-        if status_code in status_codes:
-            status_codes[status_code] += 1
-        else:
-            status_codes[status_code] = 1
-
-    except (ValueError, IndexError):
-        pass  # Ignore lines with incorrect format
-
-    return total_size, status_codes
-
-if __name__ == "__main__":
-    total_size = 0
-    status_codes = {}
-
-    try:
-        for i, line in enumerate(sys.stdin, 1):
-            total_size, status_codes = parse_line(line.strip(), total_size, status_codes)
-
-            if i % 10 == 0:
-                print_metrics(total_size, status_codes)
-
+        for line in sys.stdin:
+            fields = list(map(str, line.strip().split(" ")))
+            size += int(fields[-1])
+            if fields[-2] in stat_codes:
+                stat_codes[fields[-2]] += 1
+            lines += 1
+            if lines % 10 == 0:
+                print_size_and_codes(size, stat_codes)
     except KeyboardInterrupt:
-        print_metrics(total_size, status_codes)
+        print_size_and_codes(size, stat_codes)
         raise
+
+    print_size_and_codes(size, stat_codes)
+
+
+parse_stdin_and_compute()

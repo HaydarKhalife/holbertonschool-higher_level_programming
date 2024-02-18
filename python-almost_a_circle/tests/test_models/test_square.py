5626c02e529f38c81fa75fc381424f97427c965e
HaydarKhalife <146635050+HaydarKhalife@users.noreply.github.com>
2024-02-18 17:13:10 +0200

    new

--- python-almost_a_circle/tests/test_models/test_square.py
+++ python-almost_a_circle/tests/test_models/test_square.py
@@ -1,179 +1,113 @@
 #!/usr/bin/python3
-"""import"""
+"""
+A module that test differents behaviors
+of the Square class
+"""
 import unittest
+import pep8
+from models.base import Base
+from models.rectangle import Rectangle
 from models.square import Square
-import os
 
 
 class TestSquare(unittest.TestCase):
-    """class Rectangle tests"""
+    """
+    A class to test the Square Class
+    """
+    def test_pep8_base(self):
+        """
+        Test that checks PEP8
+        """
+        syntax = pep8.StyleGuide(quit=True)
+        check = syntax.check_files(['models/square.py'])
+        self.assertEqual(
+            check.total_errors, 0,
+            "Found code style errors (and warnings)."
+        )
 
-    def test_Square_creation_1(self):
-        square = Square(1)
-        self.assertEqual(square.size, 1)
+    def test_getter(self):
+        r1 = Square(5)
+        self.assertEqual(r1.size, 5)
 
-    def test_Square_creation_2(self):
-        square = Square(1, 2)
-        self.assertEqual(square.size, 1)
-        self.assertEqual(square.x, 2)
+    def test_setter(self):
+        r1 = Square(5)
+        r1.size = 8
+        self.assertEqual(r1.size, 8)
 
-    def test_Square_creation_3(self):
-        square = Square(1, 2, 3)
-        self.assertEqual(square.size, 1)
-        self.assertEqual(square.x, 2)
-        self.assertEqual(square.y, 3)
+    def test_string(self):
+        r1 = Square(3)
 
-    def test_Square_size_raise_type_error(self):
         with self.assertRaises(TypeError):
-            Square("1")
+            r1.size = "Hi"
 
-    def test_Square_x_raise_type_error(self):
+    def test_negative(self):
+        r1 = Square(6)
+
+        with self.assertRaises(ValueError):
+            r1.size = -5
+
+    def test_zero(self):
+        r1 = Square(6)
+
+        with self.assertRaises(ValueError):
+            r1.size = 0
+
+    def test_decimal(self):
+        r1 = Square(6)
+
         with self.assertRaises(TypeError):
-            Square(1, "2")
+            r1.size = 1.5
 
-    def test_Square_y_raise_type_error(self):
+    def test_tuple(self):
+        r1 = Square(7)
+
         with self.assertRaises(TypeError):
-            Square(1, 2, "3")
+            r1.size = (2, 8)
 
-    def test_Square_creation_4(self):
-        square = Square(1, 2, 3, 4)
-        self.assertEqual(square.size, 1)
-        self.assertEqual(square.x, 2)
-        self.assertEqual(square.y, 3)
-        self.assertEqual(square.id, 4)
+    def test_empty(self):
+        r1 = Square(7)
 
-    def test_Square_size_raise_value_error(self):
-        with self.assertRaises(ValueError):
-            Square(-1)
+        with self.assertRaises(TypeError):
+            r1.size = ''
 
-    def test_Square_x_raise_value_error(self):
-        with self.assertRaises(ValueError):
-            Square(1, -2)
+    def test_none(self):
+        r1 = Square(5)
 
-    def test_Square_y_raise_value_error(self):
-        with self.assertRaises(ValueError):
-            Square(1, 2, -3)
+        with self.assertRaises(TypeError):
+            r1.size = None
 
-    def test_Square_size_raise_value_error_2(self):
-        with self.assertRaises(ValueError):
-            Square(0)
+    def test_list(self):
+        r1 = Square(4)
 
-    def test_Square_representation(self):
-        square_repr = str(Square(1, 2, 3, 4))
-        result = '[Square] (4) 2/3 - 1'
-        self.assertEqual(square_repr, result)
+        with self.assertRaises(TypeError):
+            r1.size = [4, 7]
 
-    def test_Square_to_dictionary_exists(self):
-        square_dict = Square(1, 2, 3, 4).to_dictionary()
-        result = {
-            'size': 1,
-            'x': 2,
-            'y': 3,
-            'id': 4
-        }
-        self.assertEqual(square_dict, result)
+    def test_dict(self):
+        r1 = Square(5)
 
-    def test_Square_update_exists_1(self):
-        square = Square(4, 3, 2, 1)
-        square.update(89)
-        self.assertEqual(square.id, 89)
+        with self.assertRaises(TypeError):
+            r1.size = {"hi": 5, "world": 8}
 
-    def test_Square_update_exists_2(self):
-        square = Square(4, 3, 2, 1)
-        square.update(89, 1)
-        self.assertEqual(square.id, 89)
-        self.assertEqual(square.size, 1)
+    def test_width(self):
+        r1 = Square(5)
+        r1.size = 6
+        self.assertEqual(r1.width, 6)
+        self.assertEqual(r1.height, 6)
 
-    def test_Square_update_exists_3(self):
-        square = Square(4, 3, 2, 1)
-        square.update(89, 1, 2)
-        self.assertEqual(square.id, 89)
-        self.assertEqual(square.size, 1)
-        self.assertEqual(square.x, 2)
+    def test_to_dictionary(self):
+        Base._Base__nb_objects = 0
 
-    def test_Square_update_exists_4(self):
-        square = Square(4, 3, 2, 1)
-        square.update(89, 1, 2, 3)
-        self.assertEqual(square.id, 89)
-        self.assertEqual(square.size, 1)
-        self.assertEqual(square.x, 2)
-        self.assertEqual(square.y, 3)
+        s1 = Square(10, 2, 1, 9)
+        s1_dictionary = s1.to_dictionary()
+        expected = {'id': 9, 'x': 2, 'size': 10, 'y': 1}
+        self.assertEqual(s1_dictionary, expected)
 
-    def test_Square_update_exists_6(self):
-        square = Square(4, 3, 2, 1)
-        square.update(**{'id': 89})
-        self.assertEqual(square.id, 89)
+        s1 = Square(1, 0, 0, 9)
+        s1_dictionary = s1.to_dictionary()
+        expected = {'id': 9, 'x': 0, 'size': 1, 'y': 0}
+        self.assertEqual(s1_dictionary, expected)
 
-    def test_Square_update_exists_7(self):
-        square = Square(4, 3, 2, 1)
-        square.update(**{'id': 89, 'size': 1})
-        self.assertEqual(square.id, 89)
-        self.assertEqual(square.size, 1)
-
-    def test_Square_update_exists_8(self):
-        square = Square(4, 3, 2, 1)
-        square.update(**{'id': 89, 'size': 1, 'x': 2})
-        self.assertEqual(square.id, 89)
-        self.assertEqual(square.size, 1)
-        self.assertEqual(square.x, 2)
-
-    def test_Square_update_exists_9(self):
-        square = Square(4, 3, 2, 1)
-        square.update(**{'id': 89, 'size': 1, 'x': 2, 'y': 3})
-        self.assertEqual(square.id, 89)
-        self.assertEqual(square.size, 1)
-        self.assertEqual(square.x, 2)
-        self.assertEqual(square.y, 3)
-
-    def test_Square_create_exists_2(self):
-        square = Square.create(**{'id': 89, 'size': 1})
-        self.assertEqual(square.id, 89)
-        self.assertEqual(square.size, 1)
-
-    def test_Square_create_exists_3(self):
-        square = Square.create(**{'id': 89, 'size': 1, 'x': 2})
-        self.assertEqual(square.id, 89)
-        self.assertEqual(square.size, 1)
-        self.assertEqual(square.x, 2)
-
-    def test_Square_create_exists_4(self):
-        square = Square.create(**{'id': 89, 'size': 1, 'x': 2, 'y': 3})
-        self.assertEqual(square.id, 89)
-        self.assertEqual(square.size, 1)
-        self.assertEqual(square.x, 2)
-        self.assertEqual(square.y, 3)
-
-    def test_Square_save_to_file_exists_none(self):
-        Square.save_to_file(None)
-
-        with open('Square.json', 'r') as f:
-            self.assertEqual(f.read(), '[]')
-        os.remove('Square.json')
-
-    def test_Square_save_to_file_exists_empty(self):
-        Square.save_to_file([])
-
-        with open('Square.json', 'r') as f:
-            self.assertEqual(f.read(), '[]')
-        os.remove('Square.json')
-
-    def test_Square_save_to_file_exists(self):
-        Square.save_to_file([Square(1)])
-
-        with open('Square.json', 'r') as f:
-            self.assertEqual(
-                f.read(), '[{"id": 28, "size": 1, "x": 0, "y": 0}]')
-        os.remove('Square.json')
-
-    def test_Square_load_from_file_not_exists(self):
-        Square.save_to_file([])
-        self.assertEqual(Square.load_from_file(), [])
-
-    def test_rectangle_load_from_file_exists(self):
-        Square.save_to_file([Square(1, 1, 1, 5)])
-        lst_obj = Square.load_from_file()
-        self.assertEqual(lst_obj[0].size, 1)
-
-
-if __name__ == "__main__":
-    unittest.main()
+        s1.update(5, 5, 5, 5)
+        s1_dictionary = s1.to_dictionary()
+        expected = {'id': 5, 'x': 5, 'size': 5, 'y': 5}
+        self.assertEqual(s1_dictionary, expected)
